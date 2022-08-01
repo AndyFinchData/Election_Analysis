4fe0d8da73433f34b6f80d0191b1deca103fc7e8
Andy Finch <andyfinch17@gmail.com>
2022-08-01 14:16:44 -0500

    Got % of each candidates votes of total

--- PyPoll.py
+++ PyPoll.py
@@ -11,6 +11,9 @@ total_votes = 0
 # Candidate Options
 candidate_options = []
 
+#Declare an empty dictionary:
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -28,15 +31,31 @@ with open(file_to_load) as election_data:
 
         #If the candidate name does not match any existing candidate...
         if candidate_name not in candidate_options:
-                #Add it to the list of candidates.
-                candidate_options.append(candidate_name)
+            #Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            #2 Begin tracking each candidate's vote count
+            candidate_votes[candidate_name] = 0
+        # Add a vote to the candidate's count
+        candidate_votes[candidate_name] += 1
 
 
 # Print the candidate list.
 print(candidate_options)
+print(candidate_votes)
 
 
 # 3. The percentage of votes each candidate won
+#Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    #Retrieve vote coiunt of a candidate.
+    votes = candidate_votes[candidate_name]
+    #calculate percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # Print candidate name and percentage of votes
+    print(f'{candidate_name}: received {vote_percentage}% of the vote.')
+    
+
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 

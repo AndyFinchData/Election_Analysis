b7dede0bbb93a0022c2afc2d7fc07801befe8e49
Andy Finch <andyfinch17@gmail.com>
2022-08-01 14:02:38 -0500

    Added unique list of candidates

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,3 @@
-# Add our dependencies.
 import csv
 import os
 # Assign a variable to load a file from a path.
@@ -6,23 +5,37 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the election results and read the file.
-
-
-# 1. The total number of votes cast
+# Initialize a total vote counter.
 total_votes = 0
 
+# Candidate Options
+candidate_options = []
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
+    # Read the header row.
     headers = next(file_reader)
 
+    # Print each row in the CSV file.
     for row in file_reader:
+        # Add to the total vote count.
         total_votes += 1
 
-print(total_votes)
+        # Print the candidate name from each row.
+        candidate_name = row[2]
 
-# 2. A complete list of candidates who received votes
+        #If the candidate name does not match any existing candidate...
+        if candidate_name not in candidate_options:
+                #Add it to the list of candidates.
+                candidate_options.append(candidate_name)
+
+
+# Print the candidate list.
+print(candidate_options)
+
+
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
